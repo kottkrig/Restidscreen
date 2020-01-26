f41b00bcf95beda207bebf3545a091549ffe568d
Johan Larsson <johan@johanl.se>
2020-01-26 10:26:45 +0100

    Move all device config to device.exs

--- config/device.exs
+++ config/device.exs
@@ -1,12 +1,21 @@
 use Mix.Config
 
+# Customize non-Elixir parts of the firmware. See
+# https://hexdocs.pm/nerves/advanced-configuration.html for details.
 config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
+
+# Set the SOURCE_DATE_EPOCH date for reproducible builds.
+# See https://reproducible-builds.org/docs/source-date-epoch/ for more information
+config :nerves, source_date_epoch: "1578826697"
+
+# Use Ringlogger as the logger backend and remove :console.
+# See https://hexdocs.pm/ring_logger/readme.html for more information on
+# configuring ring_logger.
+config :logger, backends: [RingLogger]
 
 config :shoehorn,
   init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
-
-config :logger, backends: [RingLogger]
 
 keys =
   [
