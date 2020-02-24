25ad9751a670a3f6a5658b2a90a620fbf5d327f0
Johan Larsson <johan@johanl.se>
2020-02-24 20:39:28 +0100

    Return status tuple from parse_from_json

--- lib/restid/response.ex
+++ lib/restid/response.ex
@@ -2,7 +2,7 @@ defmodule Restid.Response do
   defstruct [:server_date_time, :trips]
 
   def parse_result({:ok, json}) do
-    {:ok, parse_from_json(json)}
+    parse_from_json(json)
   end
 
   def parse_result({:error, %Tesla.Error{message: message}}) do
@@ -14,18 +14,21 @@ defmodule Restid.Response do
   end
 
   def parse_from_json(json) do
-    server_date_time =
-      NaiveDateTime.from_iso8601!(
-        "#{json["TripList"]["serverdate"]} #{json["TripList"]["servertime"]}:00"
-      )
+    date_time_string = "#{json["TripList"]["serverdate"]} #{json["TripList"]["servertime"]}:00"
 
-    %Restid.Response{
-      server_date_time: server_date_time,
-      trips:
-        Enum.map(
-          json["TripList"]["Trip"],
-          &Restid.Response.Trip.parse_from_json(&1, server_date_time)
-        )
-    }
+    with {:ok, server_date_time} <- NaiveDateTime.from_iso8601(date_time_string) do
+      {:ok,
+       %Restid.Response{
+         server_date_time: server_date_time,
+         trips:
+           Enum.map(
+             json["TripList"]["Trip"],
+             &Restid.Response.Trip.parse_from_json(&1, server_date_time)
+           )
+       }}
+    else
+      {:error, message} ->
+        {:error, message}
+    end
   end
 end
